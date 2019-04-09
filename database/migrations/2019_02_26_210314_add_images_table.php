a636acfe8bdba82d9cbc7cbac36ceebd82c9c360
Kevin Cisnero <kevingabriel18@gmail.com>
2019-04-09 15:52:58 -0400

    migraciones

--- database/migrations/2019_02_26_210314_add_images_table.php
+++ database/migrations/2019_02_26_210314_add_images_table.php
@@ -1,9 +1,10 @@
 <?php
 
+use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateUsersTable extends Migration
+class AddImagesTable extends Migration
 {
     /**
      * Run the migrations.
@@ -12,15 +13,13 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('images', function (Blueprint $table) {
             $table->increments('id');
             $table->string('name');
-            $table->string('email')->unique();
-            $table->string('password', 60);
-            $table->enum('type',['admin','member'])->default('member');
-            $table->string('slug')->nullable()->unique();;
-            $table->rememberToken();
+            $table->integer('article_id')->unsigned();
             $table->timestamps();
+
+            $table->foreign('article_id')->references('id')->on('articles')->onDelete('cascade');
         });
     }
 
@@ -31,6 +30,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::drop('users');
+        Schema::dropIfExists('images');
     }
 }
